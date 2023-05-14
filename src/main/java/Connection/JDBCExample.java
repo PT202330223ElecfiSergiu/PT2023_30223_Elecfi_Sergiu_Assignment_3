6577b2a8b8307fa42156af719507e963b3e1f4af
MonkeYzPGL <elecfis@gmail.com>
2023-05-14 20:08:21 +0300

    initial commit

---
+++ src/main/java/Connection/JDBCExample.java
@@ -0,0 +1,27 @@
+package Connection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCExample {
+
+    public static void main(String[] args) {
+
+        // auto close connection
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost/tema3pt", "root", "sergiu19A@")) {
+
+            if (conn != null) {
+                System.out.println("Connected to the database!");
+            } else {
+                System.out.println("Failed to make connection!");
+            }
+
+        } catch (SQLException e) {
+            System.err.format("SQL State: %s\n%s", e.getSQLState(), e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
